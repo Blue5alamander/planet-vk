7855b8e5ead6611a0af0bd27e24c5e333ce6eaaa
Kirit Sælensminde <kirit@felspar.com>
2022-12-11 17:35:31 +0700

    Add some headers

--- include/planet/vk.hpp
+++ include/planet/vk.hpp
@@ -5,4 +5,6 @@
 #include <planet/vk/extensions.hpp>
 #include <planet/vk/helpers.hpp>
 #include <planet/vk/instance.hpp>
+#include <planet/vk/physical_device.hpp>
+#include <planet/vk/surface.hpp>
 #include <planet/vk/swap_chain.hpp>
