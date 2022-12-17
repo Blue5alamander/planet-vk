2e37d065303d868130c5a1d63e2a08efe1782bae
Kirit Sælensminde <kirit@kirit.com>
2022-12-17 19:20:30 +0700

    Set up the graphics pipeline

--- include/planet/vk/engine2d/renderer.hpp
+++ include/planet/vk/engine2d/renderer.hpp
@@ -8,10 +8,15 @@ namespace planet::vk::engine2d {
 
 
     class renderer final {
+        vk::graphics_pipeline create_pipeline();
+
       public:
         renderer(engine2d::app &);
 
         engine2d::app &app;
+
+        vk::swap_chain swapchain{app.device, app.window.extents()};
+        vk::graphics_pipeline pipeline{create_pipeline()};
     };
 
 
--- src/renderer.engine2d.cpp
+++ src/renderer.engine2d.cpp
@@ -1,4 +1,129 @@
 #include <planet/vk/engine2d/renderer.hpp>
 
 
-planet::vk::engine2d::renderer::renderer(engine2d::app &a) : app{a} {}
+planet::vk::engine2d::renderer::renderer(engine2d::app &a) : app{a} {
+    swapchain.create_frame_buffers(pipeline.render_pass);
+}
+
+
+planet::vk::graphics_pipeline planet::vk::engine2d::renderer::create_pipeline() {
+    planet::vk::shader_module vertex_shader_module{
+            app.device, app.asset_manager.file_data("2d-example.vert.spirv")};
+    planet::vk::shader_module fragment_shader_module{
+            app.device, app.asset_manager.file_data("2d-example.frag.spirv")};
+    std::array shader_stages{
+            vertex_shader_module.shader_stage_info(
+                    VK_SHADER_STAGE_VERTEX_BIT, "main"),
+            fragment_shader_module.shader_stage_info(
+                    VK_SHADER_STAGE_FRAGMENT_BIT, "main")};
+
+    // Vertex data hard-coded in vertex shader
+    VkPipelineVertexInputStateCreateInfo vertex_input_info = {};
+    vertex_input_info.sType =
+            VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    vertex_input_info.vertexBindingDescriptionCount = 0;
+    vertex_input_info.vertexAttributeDescriptionCount = 0;
+
+    // Primitive type
+    VkPipelineInputAssemblyStateCreateInfo input_assembly = {};
+    input_assembly.sType =
+            VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+    input_assembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+    input_assembly.primitiveRestartEnable = VK_FALSE;
+
+    // Viewport config
+    VkViewport viewport = {};
+    viewport.x = 0.0f;
+    viewport.y = 0.0f;
+    viewport.width = app.window.zwidth();
+    viewport.height = app.window.zheight();
+    viewport.minDepth = 0.0f;
+    viewport.maxDepth = 1.0f;
+
+    // Scissor rect config
+    VkRect2D scissor = {};
+    scissor.offset.x = 0;
+    scissor.offset.y = 0;
+    scissor.extent = swapchain.extents;
+
+    VkPipelineViewportStateCreateInfo viewport_state_info = {};
+    viewport_state_info.sType =
+            VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    viewport_state_info.viewportCount = 1;
+    viewport_state_info.pViewports = &viewport;
+    viewport_state_info.scissorCount = 1;
+    viewport_state_info.pScissors = &scissor;
+
+    VkPipelineRasterizationStateCreateInfo rasterizer_info = {};
+    rasterizer_info.sType =
+            VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    rasterizer_info.depthClampEnable = VK_FALSE;
+    rasterizer_info.rasterizerDiscardEnable = VK_FALSE;
+    rasterizer_info.polygonMode = VK_POLYGON_MODE_FILL;
+    rasterizer_info.lineWidth = 1.f;
+    rasterizer_info.cullMode = VK_CULL_MODE_BACK_BIT;
+    rasterizer_info.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    rasterizer_info.depthBiasEnable = VK_FALSE;
+
+    VkPipelineMultisampleStateCreateInfo multisampling = {};
+    multisampling.sType =
+            VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    multisampling.sampleShadingEnable = VK_FALSE;
+    multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+
+    VkPipelineColorBlendAttachmentState blend_mode = {};
+    blend_mode.colorWriteMask = VK_COLOR_COMPONENT_R_BIT
+            | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT
+            | VK_COLOR_COMPONENT_A_BIT;
+    blend_mode.blendEnable = VK_FALSE;
+
+    VkPipelineColorBlendStateCreateInfo blend_info = {};
+    blend_info.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    blend_info.logicOpEnable = VK_FALSE;
+    blend_info.attachmentCount = 1;
+    blend_info.pAttachments = &blend_mode;
+
+    VkAttachmentDescription color_attachment = {};
+    color_attachment.format = swapchain.image_format;
+    color_attachment.samples = VK_SAMPLE_COUNT_1_BIT;
+    color_attachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    color_attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    color_attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    color_attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    color_attachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    color_attachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    VkAttachmentReference color_attachment_ref = {};
+    color_attachment_ref.attachment = 0;
+    color_attachment_ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass = {};
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass.colorAttachmentCount = 1;
+    subpass.pColorAttachments = &color_attachment_ref;
+
+    VkRenderPassCreateInfo render_pass_info = {};
+    render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    render_pass_info.attachmentCount = 1;
+    render_pass_info.pAttachments = &color_attachment;
+    render_pass_info.subpassCount = 1;
+    render_pass_info.pSubpasses = &subpass;
+    planet::vk::render_pass render_pass{app.device, render_pass_info};
+
+    VkGraphicsPipelineCreateInfo graphics_pipeline_info = {};
+    graphics_pipeline_info.sType =
+            VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+    graphics_pipeline_info.stageCount = 2;
+    graphics_pipeline_info.pStages = shader_stages.data();
+    graphics_pipeline_info.pVertexInputState = &vertex_input_info;
+    graphics_pipeline_info.pInputAssemblyState = &input_assembly;
+    graphics_pipeline_info.pViewportState = &viewport_state_info;
+    graphics_pipeline_info.pRasterizationState = &rasterizer_info;
+    graphics_pipeline_info.pMultisampleState = &multisampling;
+    graphics_pipeline_info.pColorBlendState = &blend_info;
+    graphics_pipeline_info.subpass = 0;
+
+    return planet::vk::graphics_pipeline{
+            app.device, graphics_pipeline_info, std::move(render_pass),
+            planet::vk::pipeline_layout{app.device}};
+}
