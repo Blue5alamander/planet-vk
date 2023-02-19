92f3b0aa09ff5ac2a8b280d73c73a5b63330b949
Kirit Sælensminde <kirit@felspar.com>
2023-02-19 17:54:13 +0700

    Add descriptor pool

--- include/planet/vk/descriptors.hpp
+++ include/planet/vk/descriptors.hpp
@@ -10,6 +10,20 @@ namespace planet::vk {
     class device;
 
 
+    /// ## Descriptor pool
+    class descriptor_pool final {
+        using handle_type =
+                device_handle<VkDescriptorPool, vkDestroyDescriptorPool>;
+        handle_type handle;
+
+      public:
+        descriptor_pool(vk::device const &, std::uint32_t count);
+
+        vk::device const &device;
+        VkDescriptorPool get() const noexcept { return handle.get(); }
+    };
+
+
     /// ## Descriptor set layout
     class descriptor_set_layout final {
         using handle_type =
--- src/descriptors.cpp
+++ src/descriptors.cpp
@@ -2,6 +2,24 @@
 #include <planet/vk/device.hpp>
 
 
+/// ## `planet::vk::descriptor_pool`
+
+
+planet::vk::descriptor_pool::descriptor_pool(
+        vk::device const &d, std::uint32_t const count)
+: device{d} {
+    VkDescriptorPoolSize size{};
+    size.type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    size.descriptorCount = count;
+    VkDescriptorPoolCreateInfo info{};
+    info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    info.poolSizeCount = 1;
+    info.pPoolSizes = &size;
+    info.maxSets = count;
+    handle.create<vkCreateDescriptorPool>(device.get(), info);
+}
+
+
 /// ## `planet::vk::descriptor_set_layout`
 
 
