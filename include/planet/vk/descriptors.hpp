84e40aaef94a2c6d40bd31a2a2a03b359288d67a
Kirit Sælensminde <kirit@felspar.com>
2023-02-25 10:07:15 +0700

    Create the descriptor sets

--- include/planet/vk/descriptors.hpp
+++ include/planet/vk/descriptors.hpp
@@ -19,12 +19,15 @@ namespace planet::vk {
         handle_type handle;
 
       public:
+        /// ### General purpose creation of a pool for the described sizes
         descriptor_pool(
                 vk::device const &,
                 std::span<VkDescriptorPoolSize const>,
                 std::uint32_t max_sets);
+        /// ### Create a pool for a single size with the requested maximum size
         descriptor_pool(vk::device const &, std::uint32_t count);
 
+        vk::device const &device;
         VkDescriptorPool get() const noexcept { return handle.get(); }
     };
 
@@ -36,15 +39,37 @@ namespace planet::vk {
         handle_type handle;
 
       public:
+        /// ### General purpose set layout creation
         descriptor_set_layout(
                 vk::device const &, VkDescriptorSetLayoutCreateInfo const &);
+        /// ### Create a set layout for only a single binding
         descriptor_set_layout(
                 vk::device const &, VkDescriptorSetLayoutBinding const &);
 
+        vk::device const &device;
         VkDescriptorSetLayout get() const noexcept { return handle.get(); }
         VkDescriptorSetLayout const *address() const noexcept {
             return handle.address();
         }
+    };
+
+
+    /// ## Descriptor sets
+    class descriptor_sets final {
+        std::vector<VkDescriptorSet> sets;
+
+      public:
+        /// ### Create sets for a single layout, but multiple frames
+        descriptor_sets(
+                descriptor_pool const &,
+                descriptor_set_layout const &,
+                std::uint32_t count = 1);
+
+        /// ### Access to individual sets
+        auto const &operator[](std::uint32_t const index) const {
+            return sets.at(index);
+        }
+        std::size_t size() const noexcept { return sets.size(); }
     };
 
 
--- src/descriptors.cpp
+++ src/descriptors.cpp
@@ -6,9 +6,10 @@
 
 
 planet::vk::descriptor_pool::descriptor_pool(
-        vk::device const &device,
+        vk::device const &d,
         std::span<VkDescriptorPoolSize const> sizes,
-        std::uint32_t const max) {
+        std::uint32_t const max)
+: device{d} {
     VkDescriptorPoolCreateInfo info{};
     info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
     info.poolSizeCount = sizes.size();
@@ -20,7 +21,8 @@ planet::vk::descriptor_pool::descriptor_pool(
 
 
 planet::vk::descriptor_pool::descriptor_pool(
-        vk::device const &device, std::uint32_t const count) {
+        vk::device const &d, std::uint32_t const count)
+: device{d} {
     VkDescriptorPoolSize size{};
     size.type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
     size.descriptorCount = count;
@@ -38,17 +40,39 @@ planet::vk::descriptor_pool::descriptor_pool(
 
 
 planet::vk::descriptor_set_layout::descriptor_set_layout(
-        vk::device const &device, VkDescriptorSetLayoutCreateInfo const &info) {
+        vk::device const &d, VkDescriptorSetLayoutCreateInfo const &info)
+: device{d} {
     handle.create<vkCreateDescriptorSetLayout>(device.get(), info);
 }
 
 
 planet::vk::descriptor_set_layout::descriptor_set_layout(
-        vk::device const &device, VkDescriptorSetLayoutBinding const &layout) {
+        vk::device const &d, VkDescriptorSetLayoutBinding const &layout)
+: device{d} {
     VkDescriptorSetLayoutCreateInfo info{};
     info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
     info.bindingCount = 1;
     info.pBindings = &layout;
 
     handle.create<vkCreateDescriptorSetLayout>(device.get(), info);
+}
+
+
+/// ## `planet::vk::descriptor_sets`
+
+
+planet::vk::descriptor_sets::descriptor_sets(
+        descriptor_pool const &pool,
+        descriptor_set_layout const &layout,
+        std::uint32_t const count)
+: sets(count) {
+    std::vector<VkDescriptorSetLayout> layouts(count, layout.get());
+    VkDescriptorSetAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+    allocInfo.descriptorPool = pool.get();
+    allocInfo.descriptorSetCount = layouts.size();
+    allocInfo.pSetLayouts = layouts.data();
+
+    planet::vk::worked(vkAllocateDescriptorSets(
+            pool.device.get(), &allocInfo, sets.data()));
 }
