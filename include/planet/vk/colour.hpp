9a14228cd431c54c22f8ed8a64e367345814344f
Kirit Sælensminde <kirit@felspar.com>
2023-02-26 12:50:38 +0700

    A colour can be converted to a VkClearValue

--- include/planet/vk/colour.hpp
+++ include/planet/vk/colour.hpp
@@ -1,10 +1,24 @@
 #pragma once
 
 
+#include <vulkan/vulkan.h>
+
+
 namespace planet::vk {
 
 
     struct colour {
         float r{}, g{}, b{}, a = {1};
+
+        operator VkClearValue() const noexcept {
+            VkClearValue c;
+            c.color.float32[0] = r;
+            c.color.float32[1] = g;
+            c.color.float32[2] = b;
+            c.color.float32[3] = a;
+            return c;
+        }
     };
+
+
 }
