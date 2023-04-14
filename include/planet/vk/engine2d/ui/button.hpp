c9858c9a563a715dcb497298424c0f4c44e2c0fd
Kirit Sælensminde <kirit@felspar.com>
2023-04-14 16:44:49 +0700

    Allow the button graphic type to be set

--- include/planet/vk/engine2d/ui/button.hpp
+++ include/planet/vk/engine2d/ui/button.hpp
@@ -9,20 +9,25 @@
 namespace planet::vk::engine2d::ui {
 
 
-    template<typename R>
+    template<typename R, typename G = pipeline::on_screen>
     class button : public planet::ui::widget<renderer> {
         R press_value;
         felspar::coro::bus<R> &output_to;
 
       public:
+        using renderer_type = R;
+        using graphics_type = G;
+
+        button(felspar::coro::bus<R> &o, R v)
+        : press_value{std::move(v)}, output_to{o} {}
         button(texture text, felspar::coro::bus<R> &o, R v)
         : press_value{std::move(v)}, output_to{o}, graphic{std::move(text)} {}
 
-        affine::extents2d extents(affine::extents2d const &ex) const {
+        affine::extents2d extents(affine::extents2d const &ex) {
             return graphic.extents(ex);
         }
 
-        pipeline::on_screen graphic;
+        G graphic;
 
       private:
         void do_draw_within(
