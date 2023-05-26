f649dc1785c06eeb4220b9de6561c47db34bfbb4
Kirit Sælensminde <kirit@felspar.com>
2023-05-26 20:59:39 +0700

    Add missing headers

--- include/planet/vk/engine.hpp
+++ include/planet/vk/engine.hpp
@@ -3,5 +3,8 @@
 
 #include <planet/vk/engine/app.hpp>
 #include <planet/vk/engine/forward.hpp>
+#include <planet/vk/engine/mesh.pipeline.hpp>
 #include <planet/vk/engine/renderer.hpp>
+#include <planet/vk/engine/sprite.pipeline.hpp>
+#include <planet/vk/engine/textured.pipeline.hpp>
 #include <planet/vk/engine/ui.hpp>
