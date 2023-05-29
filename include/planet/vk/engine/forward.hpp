9630bec86f0513113f0d263ecc6127fbbbe9c109
Kirit Sælensminde <kirit@felspar.com>
2023-05-29 19:20:13 +0700

    Add more forward declarations

--- include/planet/vk/engine/forward.hpp
+++ include/planet/vk/engine/forward.hpp
@@ -8,14 +8,25 @@ namespace planet::vk::engine {
     class renderer;
 
 
+    namespace pipeline {
+
+
+        class mesh;
+        class sprite;
+        class textured;
+
+
+    }
+
+
     namespace ui {
 
 
         template<typename T>
         struct per_frame;
-
+        template<typename Pipeline = pipeline::textured>
+        struct tx;
 
     }
-
 
 }
