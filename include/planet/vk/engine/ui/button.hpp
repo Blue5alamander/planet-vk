10cce0516bf72cf0b412559186d191376b4b95a7
Kirit Sælensminde <kirit@felspar.com>
2023-10-29 20:10:13 +0700

    We need to be able to set custom output queues

--- include/planet/vk/engine/ui/button.hpp
+++ include/planet/vk/engine/ui/button.hpp
@@ -9,32 +9,34 @@
 namespace planet::vk::engine::ui {
 
 
-    template<typename R, typename G = planet::vk::engine::ui::tx<>>
-    class button final : public planet::ui::button<renderer, R, G> {
-        using superclass = planet::ui::button<renderer, R, G>;
+    template<
+            typename R,
+            typename G = planet::vk::engine::ui::tx<>,
+            typename Q = queue::pmc<R>>
+    class button final : public planet::ui::button<renderer, R, G, Q> {
+        using superclass = planet::ui::button<renderer, R, G, Q>;
 
       public:
         using value_type = R;
         using graphics_type = G;
+        using output_type = Q;
 
         using superclass::graphic;
         using superclass::move_to;
         using superclass::position;
         using superclass::reflow;
 
-        button(typename superclass::queue_type &o, value_type v)
+        button(output_type &o, value_type v)
         : superclass{"planet::vk::engine::ui::button", o, std::move(v)} {}
-        button(std::string_view const n,
-               typename superclass::queue_type &o,
-               value_type v)
+        button(std::string_view const n, output_type &o, value_type v)
         : superclass{n, o, std::move(v)} {}
-        button(graphics_type g, typename superclass::queue_type &o, value_type v)
+        button(graphics_type g, output_type &o, value_type v)
         : superclass{
                 "planet::vk::engine::ui::button", std::move(g), o,
                 std::move(v)} {}
         button(std::string_view const n,
                graphics_type g,
-               typename superclass::queue_type &o,
+               output_type &o,
                value_type v)
         : superclass{n, std::move(g), o, std::move(v)} {}
 
