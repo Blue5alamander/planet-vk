3f755a5f3477dd4ef9c9406d18207ffe6a1dd0a1
Kirit Sælensminde <kirit@felspar.com>
2023-12-28 11:56:42 +0700

    We can now render text in any colour

--- include/planet/vk/sdl/texture.hpp
+++ include/planet/vk/sdl/texture.hpp
@@ -2,6 +2,7 @@
 
 
 #include <planet/sdl/ttf.hpp>
+#include <planet/vk/colour.hpp>
 #include <planet/vk/texture.hpp>
 
 
@@ -12,6 +13,15 @@ namespace planet::vk {
 
 
 namespace planet::vk::sdl {
+
+
+    /// ## Convert a `vk::colour` to a `SDL_Color`
+    inline SDL_Color to_sdl_color(vk::colour const &c) noexcept {
+        auto const convert = [](float const v) {
+            return static_cast<std::uint8_t>(v * 255.0f);
+        };
+        return {convert(c.r), convert(c.g), convert(c.b), convert(c.a)};
+    }
 
 
     /// ## Create a Vulkan texture from an SDL surface
@@ -34,22 +44,26 @@ namespace planet::vk::sdl {
             command_pool &,
             planet::sdl::surface const &);
 
+
     /// ##  Render text and turn it into a Vulkan texture
     inline texture
             render(device_memory_allocator &staging,
                    device_memory_allocator &image,
                    command_pool &cp,
                    planet::sdl::font const &f,
-                   char const *t) {
+                   char const *t,
+                   vk::colour const &col = vk::colour::white) {
         return create_texture_without_mip_levels(
-                staging, image, cp, f.render(t));
+                staging, image, cp, f.render(t, to_sdl_color(col)));
     }
     inline texture
             render(device_memory_allocator &image,
                    command_pool &cp,
                    planet::sdl::font const &f,
-                   char const *t) {
-        return create_texture_without_mip_levels(image, cp, f.render(t));
+                   char const *t,
+                   vk::colour const &col = vk::colour::white) {
+        return create_texture_without_mip_levels(
+                image, cp, f.render(t, to_sdl_color(col)));
     }
 
 
