68ca7deb69707a77059ded91f6b9e33892dea8f1
Kirit Sælensminde <kirit@felspar.com>
2024-05-19 11:49:38 +0700

    Use the forwarding header

--- include/planet/vk/sdl/texture.hpp
+++ include/planet/vk/sdl/texture.hpp
@@ -3,13 +3,8 @@
 
 #include <planet/sdl/ttf.hpp>
 #include <planet/vk/colour.hpp>
+#include <planet/vk/forward.hpp>
 #include <planet/vk/texture.hpp>
-
-
-namespace planet::vk {
-    class command_pool;
-    class device_memory_allocator;
-}
 
 
 namespace planet::vk::sdl {
